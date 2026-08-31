ffb1f3d4e4274fcc45f92346f71b28bdf48f11de
Andrea <24-56465-1@student.aiub.edu>
2026-08-31 19:26:33 +0600

    product retrieval by category and brand
    Refactored product retrieval logic to handle categories and brands more efficiently.

--- controllers/productsController.php
+++ controllers/productsController.php
@@ -1,9 +1,23 @@
 <?php
+require_once "../models/db.php";
 
-    require "../models/db.php";
+$db = new mydb();
+$conn = $db->openConn();
 
-    $db = new mydb();
-    $conn = $db->openConn();
+$categories = $db->getAllCategories($conn);
+$brands = $db->getAllBrands($conn);
 
+$pageTitle = "All Products";
+
+if (isset($_GET['category']) && !empty($_GET['category'])) {
+    $selectedCategory = $_GET['category'];
+    $products = $db->getProductsByCategory($selectedCategory, $conn);
+    $pageTitle = "Category: " . $selectedCategory;
+} elseif (isset($_GET['brand']) && !empty($_GET['brand'])) {
+    $selectedBrand = $_GET['brand'];
+    $products = $db->getProductsByBrand($selectedBrand, $conn);
+    $pageTitle = "Brand: " . $selectedBrand;
+} else {
     $products = $db->getAllProducts($conn);
+}
 ?>
