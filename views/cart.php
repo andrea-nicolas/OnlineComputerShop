bf4666202c64d5c67cdb41ab139d3c0afde45686
Andrea <24-56465-1@student.aiub.edu>
2026-08-31 19:27:36 +0600

    Revamp cart page layout and functionality
    Updated the cart page layout to include a product browsing section and filter options, while removing the previous cart item display and payment method sections.

--- views/cart.php
+++ views/cart.php
@@ -1,11 +1,14 @@
 <?php 
+require "../controllers/productsController.php"; 
 require "../controllers/cartsController.php";
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Cart</title>
+    <meta charset="UTF-8">
+    <title>Products</title>
     <link rel="stylesheet" href="../assets/css/style.css">
+    <script src="../assets/js/products.js"></script>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -16,95 +19,84 @@ require "../controllers/cartsController.php";
     <div class="topnav">
         <a id="logo"><img src="../assets/logo.png"></a>
 
+        <div class="search-container">  
+            <input type="text" placeholder="Search..">
+            <button id="search-button"><img src="../assets/search.png"></button>
+        </div>
+
         <div class="menu-container">  
+            <a id="shopping-cart" href="cart.php"><img src="../assets/shopping-cart.png"></a>
             <a id="profile"><img src="../assets/profile.png"></a>
         </div>
     </div> 
 
-    <div class="cart">
-        <div class="shopping-cart">
-            <h1>Cart</h1><hr>
+    <section class="hero">
+        <h1>Browse Components</h1>
+        <h4><?php echo $pageTitle; ?></h4>
+    </section>
 
-            <div class="cartItems">
-                <?php if (empty($cartItems)): ?>
-                    <p>Your cart is empty.</p>
-                <?php else: ?>
-                    <?php foreach ($cartItems as $item): ?>
-                        <div class="cart-item">
-                            <img src="<?php echo $item['image_path']; ?>" class="cart-item-img">
-                            <div>
-                                <h4><?php echo $item['name']; ?></h4>
-                                <p>Quantity: <?php echo $item['quantity']; ?></p>
-                                <p>Price: <?php echo number_format($item['price'], 0); ?></p>
-                            </div>
-                        </div>
-                    <?php endforeach; ?>
-                <?php endif; ?>
-            </div>
-
-            <div class="cartFooter">
-                <a id="products" href="products.php">Continue Browsing</a>
-            </div>
+    <div class="filter-bar">
+        <div>
+            <b>Categories: </b>
+            <a href="products.php">All</a>
+            <?php if ($categories): ?>
+                <?php foreach ($categories as $cat): ?>
+                    <a href="products.php?category=<?php echo $cat['name']; ?>">
+                        <?php echo $cat['name']; ?>
+                    </a>
+                <?php endforeach; ?>
+            <?php endif; ?>
         </div>
-
-        <form method="POST" class="receipt">         
-            <div class="order-summary">
-                <h1>Order Summary</h1>
-                <hr>
-
-                <div class="summary-row">
-                    <p>Items</p>
-                    <p><?php echo $totalItems; ?></p>
-                </div>
-                <hr class="inner">
-
-                <div class="summary-row">
-                    <p>Sub-Total</p>
-                    <p><?php echo number_format($subtotal, 0); ?></p>
-                </div>
-
-                <div class="summary-row">
-                    <p>Shipping fee</p>
-                    <p><?php echo $subtotal > 0 ? number_format($shippingFee, 0) : "0"; ?></p>
-                </div>
-
-                <hr class="inner">
-
-                <div class="summary-row total">
-                    <p><b>Total</b></p>
-                    <p><b><?php echo number_format($totalPrice, 0); ?></b></p>
-                </div>
-
-            </div>
-
-            <div class="payment-method">
-                <h1>Payment Method</h1><hr>
-                <div class="paymentOptions">
-                    <label class="payment-label">
-                        <input type="radio" name="payment_method" value="cash">
-                        <img src="../assets/cash.png" alt="Cash" class="payment-img">
-                    </label>
-                    <label class="payment-label">
-                        <input type="radio" name="payment_method" value="card">
-                        <img src="../assets/card.png" alt="Card" class="payment-img">
-                    </label>
-                    <label class="payment-label">
-                        <input type="radio" name="payment_method" value="bkash">
-                        <img src="../assets/bkash.png" alt="bKash" class="payment-img">
-                    </label>
-                </div>
-
-                <?php if (!empty($orderMsg)): ?>
-                    <p class="order-alert">
-                        <?php echo $orderMsg; ?>
-                    </p>
-                <?php endif; ?>
-            </div>
-
-            <div class="checkout">
-                <button type="submit" name="checkout" id="checkout">Checkout</button>
-            </div>
-        </form>
+        <div>
+            <b>Brands: </b>
+            <?php if ($brands): ?>
+                <?php foreach ($brands as $b): ?>
+                    <a href="products.php?brand=<?php echo $b['name']; ?>">
+                        <?php echo $b['name']; ?>
+                    </a>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </div>
     </div>
+    
+    <section class="product-container">
+    <?php if ($products && $products->num_rows > 0): ?>
+        <?php foreach ($products as $product): ?>
+            <div class="product-card">
+                <div class="product-image">
+                    <img src="<?php echo $product['image_path']; ?>">
+                </div>
+
+                <div class="product-details">
+                    <h3><?php echo $product['name']; ?></h3>
+
+                    <div class="header">
+                        <h3><?php echo number_format($product['price'], 0); ?></h3>
+                        <h5>(<?php echo $product['stock']; ?> in stock)</h5>
+                    </div>
+
+                    <p><?php echo $product['description']; ?> <br> <br> 
+                        <i> <?php echo $product['manufacturer_review']; ?> </i> 
+                    </p>
+                </div>
+
+                <form method="POST">
+                    <div class="footer">
+                        <p id="qtyErr-<?php echo $product['id']; ?>"> <?php echo $qtyErr[$product['id']] ?? ""; ?> </p><br>
+                        <div class="qty">
+                            <input type="number" id="quantity-<?php echo $product['id']; ?>" name="quantity" value="0" class="qty-input" oninput="qtyCheck(<?php echo $product['id']; ?>)">
+                        </div>
+                        <div class="add-to-cart">
+                            <input type="hidden" name="product_id" value="<?php echo $product['id']; ?>">
+                            <button type="submit" name="addtocart">Add to Cart</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        <?php endforeach; ?>
+    <?php else: ?>
+        <p class="no-products">No products found for this filter.</p>
+    <?php endif; ?>
+    </section>
 </body>
 </html>
