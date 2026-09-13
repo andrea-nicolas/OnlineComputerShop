4dc1a9ec0b4791260d2b2d03b8ba53af29a4cf28
SyedaFaizaNawaar <syedanawar3@gmail.com>
2026-09-13 19:26:55 +0600

    changed

--- views/admin/layouts/footer.php
+++ views/admin/layouts/footer.php
@@ -1,7 +1,7 @@
 </div>
 
 <div class="footer">
-    Web Technologies (CSC 3215) &mdash; Project 02, Group 02, Task 2 &mdash; 23-53850-3
+    
 </div>
 
 <script src="../../assets/js/admin.js"></script>
